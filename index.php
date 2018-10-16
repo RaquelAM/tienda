5e9152aec1895080980097ffca8c77741d55c1d4
RaquelAM <raquelaguilar@ciencias.unam.mx>
2018-10-16 13:31:28 -0500

    adding bootstrap and improving design

--- index.php
+++ index.php
@@ -19,23 +19,32 @@ $woocommerce = new Client(
 <html>
 <head>
 	<title>Mi tiendita online</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-	<?php 
-	$productos = $woocommerce->get('products'); 
-	
-	foreach ($productos as $producto){
-		$html = '<div style="text-align: center; width: 500px;"><img src="'.$producto->images[0]->src.'" style="width: 100%;"><span style="display: block;">'.$producto->name.'</span></div>'.PHP_EOL;
-		$html .= '<input type="number" min="0" placeholder="cantidad" data-id="'.$producto->id.'">'.PHP_EOL;
-		$html .= '<button onclick="addToCar('.$producto->id.',\''.$producto->name.'\')">Agregar a carrito</button>'.PHP_EOL;
-    	echo $html;
-    	}
-    	//print_r($producto->images[0]->src)
-	?>
+	<section class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<?php 
+				$productos = $woocommerce->get('products'); 
+				
+				foreach ($productos as $producto){
+					$html = '<div class="col-md-6 text-center"><img src="'.$producto->images[0]->src.'" style="width: 100%;"><span style="display: block;">'.$producto->name.'</span>'.PHP_EOL;
+					$html .= '<input type="number" min="0" placeholder="cantidad" data-id="'.$producto->id.'">'.PHP_EOL;
+					$html .= '<button onclick="addToCar('.$producto->id.',\''.$producto->name.'\')">Agregar a carrito</button></div>'.PHP_EOL;
+			    	echo $html;
+			    	}
+			    	//print_r($producto->images[0]->src)
+				?>
+			</div>
+		</div>
+	</section>
+
 	<script
   src="https://code.jquery.com/jquery-3.3.1.min.js"
   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
   crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="script.js"></script>
 </body>
 </html>
